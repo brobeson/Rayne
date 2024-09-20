4d21920f84fd55972383254b0a6c915deb3d03b0
brobeson <brobeson@users.noreply.github.com>
2024-09-19 23:57:55 -0600

    Account for clock latency
    Latency in the clock functions is now removed from the benchmark
    timings.

--- pbench.py
+++ pbench.py
@@ -17,6 +17,7 @@ class Benchmark:
         self.subject: Optional[Callable] = None
         self.__run_times: List[int] = []
         self.__runs = runs
+        self.__clock_latency = 0
 
     def __enter__(self):
         return self
@@ -24,6 +25,7 @@ class Benchmark:
     def __exit__(self, exc_type, exc_value, traceback):
         if self.subject is None:
             return False
+        self.__measure_clock_latency()
         self.__run_benchmark()
         return True
 
@@ -47,10 +49,26 @@ class Benchmark:
         """
         return stdev(self.__run_times)
 
+    def __measure_clock_latency(self):
+        """
+        Measure the clock latency
+
+        It takes time to call time.perf_counter_ns(). A benchmark should not include that time
+        within; it's not part of executing the function under test. This function measures the
+        latency the same number of times as the benchmark runs, and stores the floor of the mean
+        latency. Later, the benchmark will subtract this mean latency from each timing measurement.
+        """
+        total_latency = 0
+        for _ in range(0, self.__runs):
+            start_time = time.perf_counter_ns()
+            end_time = time.perf_counter_ns()
+            total_latency += end_time - start_time
+        self.__clock_latency = total_latency // self.__runs
+
     def __run_benchmark(self):
         self.__run_times = [0 for _ in range(0, self.__runs)]
         for i in range(0, self.__runs):
             start_time = time.perf_counter_ns()
             self.subject()  # pylint:disable=not-callable
             end_time = time.perf_counter_ns()
-            self.__run_times[i] = end_time - start_time
+            self.__run_times[i] = max(end_time - start_time - self.__clock_latency, 0)
